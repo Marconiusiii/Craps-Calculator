b6def6af58dc58ba4f3779eb4ad0359db6b37e3f
Marco Salsiccia <marco.salsiccia@gmail.com>
2022-09-04 12:13:14 -0700

    Added more user functions, including setting roll amount and whether or not to power press.

--- betProgress.py
+++ betProgress.py
@@ -1,17 +1,28 @@
 import math
 
-print("Marco's Craps Bet Progression Calculator\n")
+version = "1.0"
+
+print("Craps Calculator {}\n\tBy: Marco Salsiccia".format(version))
 
 bet = int(input("What is your initial bet? >"))
-
 num = int(input("And rolling which number? >"))
+rolls = int(input("How many rolls? >"))
+power = input("Do you want to Power Press? y/n ")
 
-for roll in range(8):
-	vig = bet * 0.05
-	if vig <= 1:
-		vigPay = 1
+if power in ['y', 'Y', 'yes', 'Yes']:
+	print("Placing ${bet} on the {num} and hitting it {rolls} times; Power Press!".format(bet=bet, num=num, rolls=rolls))
+else:
+	print("Placing ${bet} on the {num} and hitting it {rolls} times; standard press.".format(bet=bet, num=num, rolls=rolls))
+
+for roll in range(rolls+1):
+	if num in [4, 10]:
+		vig = bet * 0.05
+		if vig <= 1:
+			vigPay = 1
+		else:
+			vigPay = math.floor(vig)
 	else:
-		vigPay = math.floor(vig)
+		vigPay = 0
 
 	if num in [4, 10]:
 		win = bet * 2 - vigPay
@@ -19,18 +30,26 @@ for roll in range(8):
 		win = bet//5 * 7
 	elif num in [6, 8]:
 		win = bet//6 * 7
-	print("{bet} wins {win}.".format(bet=bet, win=win))
-	nextBet = bet + win
-	addBet = 0
-	if num not in [6, 8]:
-		while nextBet % 5 != 0:
-			nextBet += 1
-			addBet += 1
+
+	if vigPay == 0:
+		print("{bet} wins {win}.".format(bet=bet, win=win))
 	else:
-		while nextBet % 6 != 0:
-			nextBet += 1
-			addBet += 1
-	if addBet > 0:
-		print("\tPower Pressing up ${}.".format(addBet))
+		print("{bet} minus the ${vig} vig wins {win}.".format(bet=bet, vig=vigPay, win=win))
+
+	if power in ['y', 'Y', 'yes', 'Yes']:
+		nextBet = bet + win
 		addBet = 0
-	bet = nextBet
+		if num not in [6, 8]:
+			while nextBet % 5 != 0:
+				nextBet += 1
+				addBet += 1
+		else:
+			while nextBet % 6 != 0:
+				nextBet += 1
+				addBet += 1
+		if addBet > 0:
+			print("\tPower Pressing up ${}.".format(addBet))
+			addBet = 0
+		bet = nextBet
+	else:
+		bet *= 2
